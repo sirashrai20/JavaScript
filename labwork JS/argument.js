ffea83f09d45e82fa6ce993a6221e2cd41205d92
sirashrai20 <136781743+sirashrai20@users.noreply.github.com>
2023-12-17 07:31:49 +0545

    Add files via upload

---
+++ labwork JS/argument.js
@@ -0,0 +1,16 @@
+function factorial(number) {
+    if (number === 0 || number === 1) {
+        return 1;
+    } else {
+        let result = 1;
+        for (let i = 2; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(1)); 
+console.log(factorial(10));
